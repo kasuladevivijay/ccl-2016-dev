ddcfafe8eda45c6caed933e658996d4ffea55382
Atul <Atul>
2016-03-26 15:55:35 +0530

    Teams, Fixtures, cmat members pages done

--- app/model/fixtures.php
+++ app/model/fixtures.php
@@ -19,8 +19,9 @@
 		$db->PConnect("$host", "$username", "$password", "$dbname");
 		
 
-		$group =  $_REQUEST['group'];
-		$sql = "SELECT * FROM public.teams where gp='" . $group . "'";
+		//$group =  $_REQUEST['group'];
+		
+		$sql = "select mno, m_date, t1.team_id team1id, t1.name team1Name, t1.symbol team1symbol, t1.logo team1logo, t2.team_id team2id, t2.name team2Name, t2.symbol team2symbol, t2.logo team2logo from t_schedule s, teams t1, teams t2 where s.team1=t1.team_id and s.team2=t2.team_id;";
 
 		//$rs = &$db->Execute($sql);
 
